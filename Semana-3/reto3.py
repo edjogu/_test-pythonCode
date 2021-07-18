2072fe530debb140987a8659a699646521becd58
edjogu <edjogu@gmail.com>
2021-07-18 18:28:29 -0500

    Add reto3.py final

--- Semana-3/reto3.py
+++ Semana-3/reto3.py
@@ -4,8 +4,9 @@
 # Date        Description
 # 01.07.21    Initial version
 # 02.07.21    Optimizado (Uso de funciones def)
-# 07.07.21    RF01 Actualizar contraseña
+# 07.07.21    RF01 Actualizar contraseña.
 # 07.07.21    RF02 Ingresar coordenadas de los tres sitios que más frecuenta (trabajo, casa, parque).
+# 18.07.21    RF03: El programa permite al usuario actualizar las coordenadas de los tres sitios más frecuentados.
 
 # --          --
 
